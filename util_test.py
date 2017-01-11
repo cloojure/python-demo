258d1b07f7e95132e1c557a37327a5faaabf251d
Alan Thompson <cloojure@gmail.com>
2017-01-11 12:36:15 -0800

    fixed tests for new namespace

--- util_test.py
+++ util_test.py
@@ -2,7 +2,7 @@
 import struct;
 import time;
 import math;
-from sam import *;
+import util;
 
 def func(x):
     return x + 1
@@ -17,15 +17,15 @@ def test_xxx():
     assert xx2 == ( 1, 2, 3 );
 
     assert 3 == len( [ 1, 2, 3] );
-    assert (3, 140000) == split_float(3.14);
-    assert (3, 141593) == split_float(3.141592654);
+    assert (3, 140000) == util.split_float(3.14);
+    assert (3, 141593) == util.split_float(3.141592654);
 
-    assert [97, 98, 99]      == str_to_bytearray(       'abc'           )
-    assert ['a', 'b', 'c']   == bytearray_to_chrarray(  [97, 98, 99]    )
-    assert 'abc'             == bytearray_to_str(       [97, 98, 99]    )
-    assert 'abc'             == chrarray_to_str(        ['a', 'b', 'c'] )
+    assert [97, 98, 99]      == util.str_to_bytearray(       'abc'           )
+    assert ['a', 'b', 'c']   == util.bytearray_to_chrarray(  [97, 98, 99]    )
+    assert 'abc'             == util.bytearray_to_str(       [97, 98, 99]    )
+    assert 'abc'             == util.chrarray_to_str(        ['a', 'b', 'c'] )
 
-    ts1 = curr_utc_time_tuple();
+    ts1 = util.curr_utc_time_tuple();
     time.sleep(0.1);
-    delta = timetup_subtract( ts1, curr_utc_time_tuple() );
+    delta = util.timetup_subtract( ts1, util.curr_utc_time_tuple() );
     assert ((0.09 < delta) and (delta < 0.11))
