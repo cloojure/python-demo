d471cb7037669ad67464821a896b0a0eda1c60dc
Alan Thompson <cloojure@gmail.com>
2017-01-11 18:35:31 -0800

    test_section_header_block working

--- pcapng_test.py
+++ pcapng_test.py
@@ -45,28 +45,15 @@ def test_pad_to_block32():
     with pytest.raises(AssertionError):
       pcapng.assert_block32_size( [1, 2, 3  ] )
 
-# def test_section_header_block():
-
-result = pcapng.section_header_block( [1,2,3] )
-print( ':len()    %r' % len(result) )
-print( ':[ 0: 4]  %s' % util.str_to_bytearray( result[0:4] ))
-print
-for ch in result[4:8]: print( ord(ch) )
-print
-print( ':[ 4: 8]  %r' % result[4:8] )
-print( ':[ 8:12]  %r' % result[8:12] )
-print( ':[12:14]  %r' % result[12:14] )
-print( ':[14:16]  %r' % result[14:16] )
-print( ':[16:24]  %r' % result[16:24] )
-print( ':[24:28]  %r' % result[24:28] )
-
-assert 28 == len(result)
-assert 0x0A0D0D0A == util.first( struct.unpack( '!l', result[0:4] ))
-assert 32 == util.first( struct.unpack( '!l', result[4:8] ))
-assert 0x1A2B3C4D == util.first( struct.unpack( '!L', result[8:12] ))
-assert 1 == util.first( struct.unpack( '!h', result[12:14] ))
-assert 0 == util.first( struct.unpack( '!h', result[14:16] ))
-assert -1 == util.first( struct.unpack( '!h', result[16:24] ))
-# assert xxx == result[24:28]
+def test_section_header_block():
+    result = pcapng.section_header_block( [1,2,3] )
+    assert 28              == len(result)
+    assert 0x0A0D0D0A      == util.first( struct.unpack( '!l', result[0:4] ))
+    assert 32              == util.first( struct.unpack( '!l', result[4:8] ))
+    assert 0x1A2B3C4D      == util.first( struct.unpack( '!L', result[8:12] ))
+    assert 1               == util.first( struct.unpack( '!h', result[12:14] ))
+    assert 0               == util.first( struct.unpack( '!h', result[14:16] ))
+    assert -1              == util.first( struct.unpack( '!q', result[16:24] ))
+    assert 32              == util.first( struct.unpack( '!l', result[24:28] ))
 
 
