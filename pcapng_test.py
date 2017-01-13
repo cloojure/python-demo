a7779c3d44706b94a4408f73b96e74633f2691cd
Alan Thompson <cloojure@gmail.com>
2017-01-12 22:13:21 -0800

    misc

--- pcapng_test.py
+++ pcapng_test.py
@@ -12,9 +12,9 @@ def test_section_header_block():
     assert type( blk_data )  == dict
 
     assert blk_data['block_type']           == 0x0A0D0D0A
-    assert blk_data['block_total_len']        == 28
-    assert blk_data['block_total_len']        == len( blk_str )
-    assert blk_data['block_total_len']        == blk_data['block_total_len_end']
+    assert blk_data['block_total_len']      == 28
+    assert blk_data['block_total_len']      == len( blk_str )
+    assert blk_data['block_total_len']      == blk_data['block_total_len_end']
     assert blk_data['byte_order_magic']     == 0x1A2B3C4D
     assert blk_data['major_version']        == 1
     assert blk_data['minor_version']        == 0
@@ -39,14 +39,14 @@ def test_interface_desc_block():
 def test_simple_pkt_block():
     blk_str   = pcapng.simple_pkt_block_create([1, 2, 3])
     blk_data  = pcapng.simple_pkt_block_decode(blk_str)
-    assert type( blk_str )                == str
-    assert type( blk_data )               == dict
-    assert blk_data['block_type']         == 0x00000003
+    assert type( blk_str )                  == str
+    assert type( blk_data )                 == dict
+    assert blk_data['block_type']           == 0x00000003
     assert blk_data['block_tot_len']        == 20
     assert blk_data['block_tot_len']        == blk_data['block_tot_len_end']
     assert blk_data['block_tot_len']        == len(blk_str)
     assert blk_data['block_tot_len']        == 16 + blk_data['pkt_data_pad_len']
-    assert blk_data['original_pkt_len']   == 3
-    assert blk_data['pkt_data']           == util.byte_list_to_str( [1,2,3] )
+    assert blk_data['original_pkt_len']     == 3
+    assert blk_data['pkt_data']             == util.byte_list_to_str( [1,2,3] )
 
 
