f054c5f41e25358a99ee2ccfb41769e9aae33ab7
Alan Thompson <cloojure@gmail.com>
2017-01-12 22:07:27 -0800

    misc

--- pcapng.py
+++ pcapng.py
@@ -1,71 +1,68 @@
 #!/usr/bin/python
-import struct;
-import time;
-import math;
-import linktype;
-import util;
+import struct
+import linktype
+import util
 
 #todo options (for all)
 
 def section_header_block_create():    #todo data_len, options
-    blk_type = 0x0A0D0D0A
+    block_type = 0x0A0D0D0A
     byte_order_magic = 0x1A2B3C4D
     major_version = 1
     minor_version = 0
     section_len = -1        #todo set to actual (incl padding)
     options_bytes=[]        #todo none at present
     options_str = util.byte_list_to_str(options_bytes)
-    blk_total_len =  ( 4 +      # block type
-                       4 +      # block total length
-                       4 +      # byte order magic
-                       2 + 2 +  # major version + minor version
-                       8 +      # section length
-                       len(options_bytes) +
-                       4 )      # block total length
-    header = ( struct.pack( '=LlLhhq', blk_type, blk_total_len, byte_order_magic,
-                                       major_version, minor_version, section_len ) +
-               options_str + 
-               struct.pack( '=l', blk_total_len ))
-    return header
+    block_total_len =    ( 4 +      # block type
+                           4 +      # block total length
+                           4 +      # byte order magic
+                           2 + 2 +  # major version + minor version
+                           8 +      # section length
+                           len(options_bytes) +
+                           4 )      # block total length
+    block = ( struct.pack( '=LlLhhq', block_type, block_total_len, byte_order_magic,
+                                      major_version, minor_version, section_len )
+            + options_str
+            + struct.pack( '=l', block_total_len ))
+    return block
 
 def section_header_block_decode(block):
     assert type( block ) == str
     block_type          = util.first( struct.unpack( '=l', block[0:4]   ))
-    blk_total_len       = util.first( struct.unpack( '=l', block[4:8]   ))
+    block_total_len     = util.first( struct.unpack( '=l', block[4:8]   ))
     byte_order_magic    = util.first( struct.unpack( '=L', block[8:12]  ))
     major_version       = util.first( struct.unpack( '=h', block[12:14] ))
     minor_version       = util.first( struct.unpack( '=h', block[14:16] ))
     section_len         = util.first( struct.unpack( '=q', block[16:24] ))
-    blk_total_len_end   = util.first( struct.unpack( '=l', block[24:28] ))
+    block_total_len_end = util.first( struct.unpack( '=l', block[24:28] ))
 
     block_data = {  'block_type'          : block_type ,
-                    'blk_total_len'       : blk_total_len ,
+                    'block_total_len'     : block_total_len ,
                     'byte_order_magic'    : byte_order_magic ,
                     'major_version'       : major_version ,
                     'minor_version'       : minor_version ,
                     'section_len'         : section_len ,
-                    'blk_total_len_end'   : blk_total_len_end }
+                    'block_total_len_end' : block_total_len_end }
     return block_data
 
 def interface_desc_block_create():
-    blk_type = 0x00000001
+    block_type = 0x00000001
     link_type = linktype.LINKTYPE_ETHERNET   # todo how determine?
     reserved = 0
     snaplen = 0                     # 0 => no limit
     options_bytes=[]                #todo none at present
     options_str = util.byte_list_to_str(options_bytes)
     util.assert_block32_size( options_bytes )
-    blk_total_len = (  4 +         # block type
-                       4 +         # block total length
-                       2 + 2 +     # linktype + reserved
-                       4 +         # snaplen
-                       len(options_bytes) +
-                       4 )         # block total length
-    header = ( struct.pack( '=LlHHl', blk_type, blk_total_len, link_type, reserved,
-                                      snaplen ) +
-               options_str + 
-               struct.pack( '=l', blk_total_len ))
-    return header
+    block_total_len =   (  4 +         # block type
+                           4 +         # block total length
+                           2 + 2 +     # linktype + reserved
+                           4 +         # snaplen
+                           len(options_bytes) +
+                           4 )         # block total length
+    block = ( struct.pack( '=LlHHl', block_type, block_total_len, link_type, reserved, snaplen )
+            + options_str
+            + struct.pack( '=l', block_total_len ))
+    return block
 
 def interface_desc_block_decode(block):
     assert type( block ) == str
@@ -90,32 +87,32 @@ def simple_pkt_block_create(pkt_data):
     assert type(pkt_data) == list
     pkt_data_pad     = util.pad_to_block32(pkt_data)
     pkt_data_pad_str = util.byte_list_to_str( pkt_data_pad )
-    blk_type = 0x00000003
+    block_type = 0x00000003
     original_pkt_len = len(pkt_data)
     pkt_data_pad_len = len(pkt_data_pad)
-    blk_total_len = ( 4 +      # block type
-                      4 +      # block total length
-                      4 +      # original packet length
-                      pkt_data_pad_len +
-                      4 )      # block total length
-    block = ( struct.pack( '=LLL', blk_type, blk_total_len, original_pkt_len ) +
+    block_total_len = ( 4 +      # block type
+                        4 +      # block total length
+                        4 +      # original packet length
+                        pkt_data_pad_len +
+                        4 )      # block total length
+    block = ( struct.pack( '=LLL', block_type, block_total_len, original_pkt_len ) +
               pkt_data_pad_str +
-              struct.pack( '=L', blk_total_len ))
+              struct.pack( '=L', block_total_len ))
     return block
 
 def simple_pkt_block_decode(block):
     assert type( block ) == str
     block_type          = util.first( struct.unpack( '=L', block[0:4]  ))
-    blk_tot_len         = util.first( struct.unpack( '=L', block[4:8]  ))
+    block_tot_len       = util.first( struct.unpack( '=L', block[4:8]  ))
     original_pkt_len    = util.first( struct.unpack( '=L', block[8:12] ))
     pkt_data_pad_len    = util.block32_pad_len( original_pkt_len )
     pkt_data            = block[ 12 : (12+original_pkt_len)  ]
-    blk_tot_len_end     = util.first( struct.unpack( '=L', block[ -4:blk_tot_len] ))
-    result = {  'block_type'          : block_type ,
-                'blk_tot_len'         : blk_tot_len ,
-                'original_pkt_len'    : original_pkt_len ,
-                'pkt_data_pad_len'    : pkt_data_pad_len ,
-                'pkt_data'            : pkt_data ,
-                'blk_tot_len_end'     : blk_tot_len_end }
-    return result
+    block_tot_len_end   = util.first( struct.unpack( '=L', block[ -4:block_tot_len] ))
+    block_data =    { 'block_type'          : block_type ,
+                      'block_tot_len'         : block_tot_len ,
+                      'original_pkt_len'    : original_pkt_len ,
+                      'pkt_data_pad_len'    : pkt_data_pad_len ,
+                      'pkt_data'            : pkt_data ,
+                      'block_tot_len_end'     : block_tot_len_end }
+    return block_data
 
--- pcapng_test.py
+++ pcapng_test.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
-import pytest;
-import struct;
-
-import linktype;
-import pcapng;
-import util;
+import pytest
+import linktype
+import util
 import pcapng
 
 
@@ -61,47 +58,47 @@ def test_pad_to_block32():
       util.assert_block32_size( [1, 2, 3  ] )
 
 def test_section_header_block():
-    block_str     = pcapng.section_header_block_create()
-    block_data    = pcapng.section_header_block_decode(block_str)
-    assert type( block_str  )  == str
-    assert type( block_data )  == dict
+    blk_str     = pcapng.section_header_block_create()
+    blk_data    = pcapng.section_header_block_decode(blk_str)
+    assert type( blk_str  )  == str
+    assert type( blk_data )  == dict
 
-    assert block_data['block_type']           == 0x0A0D0D0A
-    assert block_data['blk_total_len']        == 28
-    assert block_data['blk_total_len']        == len( block_str )
-    assert block_data['blk_total_len']        == block_data['blk_total_len_end']
-    assert block_data['byte_order_magic']     == 0x1A2B3C4D
-    assert block_data['major_version']        == 1
-    assert block_data['minor_version']        == 0
-    assert block_data['section_len']          == -1
+    assert blk_data['block_type']           == 0x0A0D0D0A
+    assert blk_data['block_total_len']        == 28
+    assert blk_data['block_total_len']        == len( blk_str )
+    assert blk_data['block_total_len']        == blk_data['block_total_len_end']
+    assert blk_data['byte_order_magic']     == 0x1A2B3C4D
+    assert blk_data['major_version']        == 1
+    assert blk_data['minor_version']        == 0
+    assert blk_data['section_len']          == -1
 
 
 def test_interface_desc_block():
-    block_str    = pcapng.interface_desc_block_create()
-    block_data   = pcapng.interface_desc_block_decode(block_str)
-    assert type( block_str )     == str
-    assert type( block_data )    == dict
+    blk_str    = pcapng.interface_desc_block_create()
+    blk_data   = pcapng.interface_desc_block_decode(blk_str)
+    assert type( blk_str )     == str
+    assert type( blk_data )    == dict
 
-    assert block_data['block_type']          == 0x00000001
-    assert block_data['block_total_len']     == 20
-    assert block_data['block_total_len']     == block_data['block_total_len_end']
-    assert block_data['block_total_len']     == len(block_str)
-    assert block_data['link_type']           == linktype.LINKTYPE_ETHERNET
-    assert block_data['reserved']            == 0
-    assert block_data['snaplen']             == 0
+    assert blk_data['block_type']          == 0x00000001
+    assert blk_data['block_total_len']     == 20
+    assert blk_data['block_total_len']     == blk_data['block_total_len_end']
+    assert blk_data['block_total_len']     == len(blk_str)
+    assert blk_data['link_type']           == linktype.LINKTYPE_ETHERNET
+    assert blk_data['reserved']            == 0
+    assert blk_data['snaplen']             == 0
 
 
 def test_simple_pkt_block():
-    block_str   = pcapng.simple_pkt_block_create([1, 2, 3])
-    block_data  = pcapng.simple_pkt_block_decode(block_str)
-    assert type( block_str )                == str
-    assert type( block_data )               == dict
-    assert block_data['block_type']         == 0x00000003
-    assert block_data['blk_tot_len']        == 20
-    assert block_data['blk_tot_len']        == block_data['blk_tot_len_end']
-    assert block_data['blk_tot_len']        == len(block_str)
-    assert block_data['blk_tot_len']        == 16 + block_data['pkt_data_pad_len']
-    assert block_data['original_pkt_len']   == 3
-    assert block_data['pkt_data']           == util.byte_list_to_str( [1,2,3] )
+    blk_str   = pcapng.simple_pkt_block_create([1, 2, 3])
+    blk_data  = pcapng.simple_pkt_block_decode(blk_str)
+    assert type( blk_str )                == str
+    assert type( blk_data )               == dict
+    assert blk_data['block_type']         == 0x00000003
+    assert blk_data['block_tot_len']        == 20
+    assert blk_data['block_tot_len']        == blk_data['block_tot_len_end']
+    assert blk_data['block_tot_len']        == len(blk_str)
+    assert blk_data['block_tot_len']        == 16 + blk_data['pkt_data_pad_len']
+    assert blk_data['original_pkt_len']   == 3
+    assert blk_data['pkt_data']           == util.byte_list_to_str( [1,2,3] )
 
 
